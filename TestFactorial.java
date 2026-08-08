54c8ffa989195f4b3644c2447289d5af2769a7a9
joshi440 <Joshithach2404@gmail.com>
2026-08-08 13:47:48 +0530

    Add files via upload

---
+++ TestFactorial.java
@@ -0,0 +1,10 @@
+public class TestFactorial {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        int result = Factorial.factorial(n);
+
+        System.out.println("Factorial of " + n + " = " + result);
+    }
+}
